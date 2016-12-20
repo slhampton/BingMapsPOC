40357d2278ec0f3d60d32772e5fc8d662dc12e1f
Sam Hampton <sam.hampton@oneadvanced.com>
2016-12-20 11:08:38 +0000

    Add data contracts

--- BingMapsPOC/UserControls/MapControl.xaml.cs
+++ BingMapsPOC/UserControls/MapControl.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media.Animation;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using Microsoft.Maps.MapControl.WPF;
 using Microsoft.Maps.MapControl.WPF.Design;
+using UserControls.Services;
+using Location = Microsoft.Maps.MapControl.WPF.Location;
+using Point = System.Windows.Point;
 
 namespace UserControls
 {
@@ -45,6 +49,59 @@ namespace UserControls
                 this.Map.SetView(center, zoom);
             }
 
+        }
+
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Map.ZoomLevel++;
+        }
+
+        private void ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            this.Map.ZoomLevel--;
+        }
+
+        private void MapWithPushpins_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // Disables the default mouse double-click action.
+            e.Handled = true;
+
+            //Get the mouse click coordinates
+            Point mousePosition = e.GetPosition(this);
+            //Convert the mouse coordinates to a locatoin on the map
+            Location pinLocation = this.Map.ViewportPointToLocation(mousePosition);
+
+            // The pushpin to add to the map.
+            Pushpin pin = new Pushpin
+            {
+                Location = pinLocation
+            };
+
+            // Adds the pushpin to the map.
+            this.Map.Children.Add(pin);
+        }
+
+        private void MapWithPushpins_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Location location;
+            var point = e.GetPosition(this);
+            this.Map.TryViewportPointToLocation(point, out location);
+
+            Ellipse dot = new Ellipse();
+            SolidColorBrush blueBrush = new SolidColorBrush();
+            blueBrush.Color = Colors.Blue;
+            dot.Fill = blueBrush;
+            double radius = 12.0;
+            dot.Width = radius * 2;
+            dot.Height = radius * 2;
+            ToolTip tt = new ToolTip();
+            tt.Content = "Location = " + location;
+            dot.ToolTip = tt;
+            Point p0 = this.Map.LocationToViewportPoint(location);
+            Point p1 = new Point(p0.X - radius, p0.Y - radius);
+            Location loc = this.Map.ViewportPointToLocation(p1);
+            MapLayer.SetPosition(dot, loc);
+            this.Map.Children.Add(dot);
         }
     }
 }
---
+++ BingMapsPOC/UserControls/Services/BingMaps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace UserControls.Services
+{
+    public class BingMaps
+    {
+        public Image AddIcon(Location location)
+        {
+            var uri =
+                new Uri(
+                    "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/47.610,-122.107/6?mapSize=100,600&pushpin=47.610,-122.107;40;P10&key=AsJ2NC7HdNc4k8T03GPuBT5IwzYUJATkSTWfaog7uEJ0lsx_XLS3st1ZStjitTm2");
+
+
+            //using (var client = new HttpClient())
+            //{
+            //    client.DefaultRequestHeaders
+            //        .Accept
+            //        .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            //    var response = client.SendAsync(request).Result;
+            //    response.EnsureSuccessStatusCode();
+
+            Image img = null;
+            HttpWebRequest request = WebRequest.Create(uri) as HttpWebRequest;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                using (Stream str = response.GetResponseStream())
+                {
+                    img = Image.FromStream(str);
+                }
+            }
+
+            return img;
+         }
+    }
+}
+
